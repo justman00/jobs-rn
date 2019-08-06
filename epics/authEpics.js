a95d774905c9db7b181a75f3cadd955ea50e3556
Vlad <vovaturcan4@gmail.com>
2019-08-06 21:55:44 +0300

    finished the auth flow

--- epics/authEpics.js
+++ epics/authEpics.js
@@ -1,46 +1,46 @@
-import { ofType } from "redux-observable";
-import * as Facebook from "expo-facebook";
-import { mergeMap, catchError } from "rxjs/operators";
-import { AsyncStorage } from "react-native";
+import { ofType } from 'redux-observable'
+import * as Facebook from 'expo-facebook'
+import { mergeMap, catchError } from 'rxjs/operators'
+import { AsyncStorage } from 'react-native'
 
 import {
   FACEBOOK_LOGIN_ATTEMPT,
   FACEBOOK_LOGIN_SUCCESS,
-  FACEBOOK_LOGIN_FAIL
-} from "../actions/types";
+  FACEBOOK_LOGIN_FAIL,
+} from '../actions/types'
 
-const AUTH_KEY = "fb_token";
+export const AUTH_KEY = 'fb_token'
 
 export function authEpic(action$, state$) {
   return action$.pipe(
     ofType(FACEBOOK_LOGIN_ATTEMPT),
     mergeMap(async action => {
-      const token = await AsyncStorage.getItem(AUTH_KEY);
+      const token = await AsyncStorage.getItem(AUTH_KEY)
 
       if (!token) {
-        throw new Error("No token found!");
+        throw new Error('No token found!')
       }
 
       // instantly authenticate our user
-      return { type: FACEBOOK_LOGIN_SUCCESS, payload: token };
+      return { type: FACEBOOK_LOGIN_SUCCESS, payload: token }
     }),
     catchError(async err => {
       // start the logging process
       const { type, token } = await Facebook.logInWithReadPermissionsAsync(
-        "499122980837665",
+        '499122980837665',
         {
-          permissions: ["public_profile"]
-        }
-      );
+          permissions: ['public_profile'],
+        },
+      )
 
-      if (type === "cancel") {
-        return { type: FACEBOOK_LOGIN_FAIL };
+      if (type === 'cancel') {
+        return { type: FACEBOOK_LOGIN_FAIL }
       } else {
-        AsyncStorage.setItem(AUTH_KEY, JSON.stringify(token));
-        return { type: FACEBOOK_LOGIN_SUCCESS, payload: token };
+        AsyncStorage.setItem(AUTH_KEY, JSON.stringify(token))
+        return { type: FACEBOOK_LOGIN_SUCCESS, payload: token }
       }
-    })
-  );
+    }),
+  )
 
   // return action$.pipe(
   //   ofType(SET_CONFIG),
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,29 +1,52 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react'
+import { AppLoading } from 'expo'
+import { AsyncStorage } from 'react-native'
 
-import Slides from "../components/Slides";
+import { AUTH_KEY } from '../epics/authEpics'
+import Slides from '../components/Slides'
+
+function WelcomeScreen({ navigation }) {
+  const [token, setToken] = React.useState(null)
+
+  React.useEffect(() => {
+    async function checkAuth() {
+      const token = await AsyncStorage.getItem(AUTH_KEY)
+
+      if (token) {
+        navigation.navigate('map')
+        setToken(token)
+      } else {
+        setToken(false)
+      }
+    }
+
+    checkAuth()
+  }, [])
+
+  const onSlidesComplete = React.useCallback(() => {
+    navigation.navigate('auth')
+  }, [])
+
+  return token === null ? (
+    <AppLoading />
+  ) : (
+    <Slides data={SLIDE_DATA} onComplete={onSlidesComplete} />
+  )
+}
+
+export default WelcomeScreen
 
 const SLIDE_DATA = [
   {
-    text: "Welcome to JobApp",
-    color: "#03a9f4"
+    text: 'Welcome to JobApp',
+    color: '#03a9f4',
   },
   {
-    text: "Use this to get a job",
-    color: "#009688"
+    text: 'Use this to get a job',
+    color: '#009688',
   },
   {
-    text: "Set your location and swipe away",
-    color: "#03a9f4"
-  }
-];
-
-function WelcomeScreen({ navigation }) {
-  const onSlidesComplete = React.useCallback(() => {
-    navigation.navigate("auth");
-  }, []);
-
-  return <Slides data={SLIDE_DATA} onComplete={onSlidesComplete} />;
-}
-
-export default WelcomeScreen;
+    text: 'Set your location and swipe away',
+    color: '#03a9f4',
+  },
+]
