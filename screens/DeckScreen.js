8bb187122bbe19c54307ebdc3cb143d25dd570c7
Vlad <vovaturcan4@gmail.com>
2019-08-07 20:58:48 +0300

    done with the app for now

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -3,10 +3,23 @@ import { Text, View } from 'react-native'
 
 function DeckScreen() {
   return (
-    <View>
-      <Text>DeckScreen</Text>
+    <View style={styles.viewStyles} >
+      <Text style={styles.textStyles} >Ecaterina Bajurea, ce faci?</Text>
     </View>
   )
+}
+
+const styles = {
+  viewStyles: {
+    flex: 1,
+    alignItem: 'center',
+    justifyContent: 'center'
+  },
+  textStyles: {
+    fontSize: 30,
+    width: 350,
+    textAlign: 'center'
+  }
 }
 
 export default DeckScreen
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,33 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
+import MapView from 'react-native-maps'
+
+const initialRegion = {
+  longitude: -122,
+  latitude: 37,
+  longitudeDelta: 0.04,
+  latitudeDelta: 0.09,
+}
 
 function MapScreen() {
-  return (
+  const [region, setRegion] = React.useState(initialRegion)
+  const [loaded, setLoaded] = React.useState(false)
+
+  React.useEffect(() => {
+    setLoaded(true)
+  }, [])
+
+  const onRegionChangeComplete = React.useCallback((region) => {
+    setRegion(region)
+  }, [region])
+
+  return !loaded ? (
     <View>
-      <Text>MapScreen</Text>
+      <ActivityIndicator size='large' />
+    </View>
+  ) : (
+    <View style={{ flex: 1 }}>
+      <MapView region={region} style={{ flex: 1 }} onRegionChangeComplete={onRegionChangeComplete} />
     </View>
   )
 }
